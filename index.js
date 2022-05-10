7f3814d25cf94d14f00d4d399f1f5f110aeffa57
Micaela Frank <micaelalfrank@gmail.com>
2022-05-10 09:38:26 -0400

    new HTML outline with meal generator container

---
+++ index.js
@@ -0,0 +1,14 @@
+
+fetch('http://localhost:3000/meals')
+	.then(response => response.json())
+	.then(json => loadRandomMeal(json));
+
+function loadRandomMeal(meals){
+    // meals.forEach((meal) =>{
+        let detailTitle = document.querySelector('#fotdTitle');
+        detailTitle.innerText = meals[15].strIngredient;
+        // let img = document.querySelector('#image');
+        // img.src = meals[0].strMealThumb;
+        let detailInstructions = document.querySelector('#fotdInstructions');
+        detailInstructions.textContent = meals[15].strDescription;
+    }
