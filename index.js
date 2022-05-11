6d3a7c1bf57e43a7169d52194a453221985b62d0
osvaldo gratereaux <osvaldo.gratereaux@gmail.com>
2022-05-11 13:50:23 -0400

    like button

--- index.js
+++ index.js
@@ -16,55 +16,37 @@ function loadRandomMeal(data){
         let mealInstructions = document.querySelector('#random-meal-instructions');
         mealInstructions.textContent = meals.strInstructions;
     }}
-        // let mealId = meals.idMeal;
-        // www.themealdb.com/api/json/v1/1/lookup.php?i=`${mealId}`
-        
-        // const ingredientList = document.querySelector('#ingredient-list');
-        
-//         const ingredientItem1 = document.createElement('li');
-//         const ingredientItem2 = document.createElement('li');
-//         const ingredientItem3 = document.createElement('li');
-//         const ingredientItem4 = document.createElement('li');
-//         const ingredientItem5 = document.createElement('li');
-//         const ingredientItem6 = document.createElement('li');
-
-//         ingredientItem1.innerText = meals.strIngredient1;
-//         ingredientItem2.innerText = meals.strIngredient2;
-//         ingredientItem3.innerText = meals.strIngredient3;
-//         ingredientItem4.innerText = meals.strIngredient4;
-//         ingredientItem5.innerText = meals.strIngredient5;
-//         ingredientItem6.innerText = meals.strIngredient6;
-//         ingredientList.append(ingredientItem1, ingredientItem2, ingredientItem3, ingredientItem4, ingredientItem5, ingredientItem6);
-//     }
-// }
 
 
+let recipeForm = document.getElementById("recipe-form");
+let recipeDescription = document.getElementById("description");
+let newRecipes = document.getElementById("new-recipes");
 
-// function renderIngredients(data){
-//     let ingredients = data.meals.slice(9, 20);
-//     ingredientItem = document.createElement('li');
-//     const ul = document.querySelector('#ingredient-list');
-
-
-
-//     ul.appendChild(ingredient);
-// }
-        
-        
-        // const ingredient1 = 
-        // const ingredientImg = document.createElement('img');
-        // const ul = document.querySelector('#ingredient-list');
-        // let i = 1;
-        // while((`meals.strIngredient${i}.innerHTML` !== "") && (i < 21)){
-        //         let ingredientNum = `meals.strIngredient${i}.textContent`;
-        //         console.log(ingredientNum);
-        //         ingredientImg.src = `www.themealdb.com/images/ingredients/${ingredientNum}.png`;
-        //         ul.append(ingredientImg);
-        //         i++;
-        //         // ingredient.src = `www.themealdb.com/images/ingredients/${ingredient}.png`;
-        //         // let ingredientImg = ingredient.src
-        //         // ul.append(ingredientImg);
-        // }
+recipeForm.addEventListener("submit",function(e){
+    e.preventDefault();
+    let newReview = document.createElement("li");
+    newReview.innerText = description.value
+    newRecipes.append(newReview)
+    e.target.reset()
+    console.log("submit")
+    //like button
+     let likeBtn = document.createElement("button");
+    //  let likeText = document.createElement("p")
+    //  likeText.innerText = "Likes"
+    //  newReview.append(likeBtn)
     
+        newReview.append(likeBtn)
+        likeBtn.innerText = 0;
+        likeBtn.addEventListener("click",incrementBtn)
+})
+    function incrementBtn(e) {
+    let num = parseInt(e.target.innerText)
+    let num2 = num + 1;
+    e.target.innerText = num2;
+}
+ 
+        
+        
+       
 
 
